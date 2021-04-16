83d68b575af00e7b9c0a6e545201fd283fccec2f
Aryan Misra <aryanmisra4@gmail.com>
2021-04-16 19:19:14 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ const cron = require("node-cron");
 import profileQuery from "./utils/query";
 cron.schedule("*/10 * * * *", () => {
   console.log("---------------------");
-  console.log("Running Cron Job");
+  console.log("Running Poller Job");
   profileQuery("BC1YLjQtaLyForGFpdzmvzCCx1zbSCm58785cABn5zS8KVMeS4Z4aNK");
 });
 
