946dfd4f7537310fd8d22c6fa0b11161d4acbd75
Aryan Misra <aryanmisra4@gmail.com>
2021-04-17 19:20:44 -0400

    Update query.ts

--- utils/query.ts
+++ utils/query.ts
@@ -19,41 +19,46 @@ const profileQuery = async (id: string) => {
       logger.info("cloutmap ", Object.keys(cloutMap));
       // if (response["Transactions"]) {
       logger.info("valid response");
-      for (const txn of response["Transactions"]) {
-        if (txn["TransactionType"] == "BASIC_TRANSFER") {
-          logger.info("found a basic transaction");
-          let output = txn["Outputs"];
-          let pastidcheck = await Transaction.find({
-            tx_id: txn["TransactionIDBase58Check"],
-          }).exec();
-          if (pastidcheck.length == 0) {
-            logger.info("unique txn found");
-            logger.info();
-            if (
-              Object.keys(cloutMap).includes(output[1].PublicKeyBase58Check)
-            ) {
-              logger.info("matching txn found");
-              let tx_ = await Transaction.findOne({
-                bitcloutpubkey: output[1].PublicKeyBase58Check,
-              }).exec();
-              if (tx_ && output[0].AmountNanos >= tx_.bitcloutnanos) {
-                logger.info("txn processed and finished");
-                let user = await User.findOne({
-                  username: tx_.username.toString(),
+      let txn_list = JSON.parse(response["Transactions"]);
+      if (txn_list) {
+        for (const txn of txn_list) {
+          if (txn["TransactionType"] == "BASIC_TRANSFER") {
+            logger.info("found a basic transaction");
+            let output = txn["Outputs"];
+            let pastidcheck = await Transaction.find({
+              tx_id: txn["TransactionIDBase58Check"],
+            }).exec();
+            if (pastidcheck.length == 0) {
+              logger.info("unique txn found");
+              logger.info();
+              if (
+                Object.keys(cloutMap).includes(output[1].PublicKeyBase58Check)
+              ) {
+                logger.info("matching txn found");
+                let tx_ = await Transaction.findOne({
+                  bitcloutpubkey: output[1].PublicKeyBase58Check,
                 }).exec();
-                tx_.status = "completed";
-                tx_.tx_id = txn["TransactionIDBase58Check"];
-                if (user) {
-                  user.bitswapbalance += output[0].AmountNanos;
-                  user.save();
+                if (tx_ && output[0].AmountNanos >= tx_.bitcloutnanos) {
+                  logger.info("txn processed and finished");
+                  let user = await User.findOne({
+                    username: tx_.username.toString(),
+                  }).exec();
+                  tx_.status = "completed";
+                  tx_.tx_id = txn["TransactionIDBase58Check"];
+                  if (user) {
+                    user.bitswapbalance += output[0].AmountNanos;
+                    user.save();
+                  }
+                  tx_.save();
                 }
-                tx_.save();
               }
+            } else {
+              logger.error("txn used previously");
             }
-          } else {
-            logger.error("txn used previously");
           }
         }
+      } else {
+        logger.error("invalid response");
       }
       // }
     })
