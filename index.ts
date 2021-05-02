75c1bf4dd89fae037386905a6f986d49d68b300d
Aryan Misra <aryanmisra4@gmail.com>
2021-05-02 02:05:15 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const config = require("./utils/config");
 const logger = require("./utils/logger");
 const cron = require("node-cron");
 import profileQuery from "./utils/query";
-cron.schedule("*/2 * * * *", async () => {
+cron.schedule("*/3 * * * *", async () => {
   console.log("---------------------");
   console.log("Running Poller Job");
   profileQuery("BC1YLiYo25DLiUf9XfNPWD4EPcuZkUTFnRCeq9RjRum1gkaYJ2K4Vu1");
