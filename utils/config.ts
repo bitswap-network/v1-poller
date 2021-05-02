2c119f25415ae2a1edb370bfe32db487553349fb
Aryan Misra <aryanmisra4@gmail.com>
2021-05-02 02:08:06 -0400

    ijm

--- utils/config.ts
+++ utils/config.ts
@@ -1,4 +1,4 @@
 require("dotenv").config();
 
 export let PORT = process.env.PORT ? process.env.PORT : 5000;
-export let MONGODB_URI = process.env.MONGODB_URI ? process.env.MONGODB_URI : "mongodb+srv://admin:irxvNRi4aYEmLYBG@bitswap.lernp.mongodb.net/bitswap";
+export let MONGODB_URI = process.env.MONGODB_URI;
